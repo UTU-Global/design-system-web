63e0cda4c64ad01a0b70fc475c355241641dfd18
salvatore.laisa <salvatore.laisa@utu.global>
2019-08-22 12:20:40 +0000

    Merged PR 2036: feature/compile-components
    add script to compile vanilla Web Components javascript files before pushing to the NPM registry.

---
+++ index.d.ts
@@ -0,0 +1,2 @@
+import './components';
+import './visual/index.scss';
---
+++ webpack.components.js
@@ -0,0 +1,48 @@
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const path = require('path');
+
+module.exports = {
+	mode: 'production',
+	entry: {
+    index: './src/components/index.ts',
+    button: './src/components/button/index.ts',
+    card: './src/components/card/index.ts',
+    datepicker: './src/components/datepicker/index.ts',
+    switch: './src/components/switch/index.ts',
+    tabs: './src/components/tabs/index.ts',
+  },
+	output: {
+    path: path.join(__dirname, './components'),
+    filename: '[name].js',
+    chunkFilename: '[name].js'
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.mjs', '.ts', '.js']
+  },
+	module: {
+		rules: [
+			{
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: [
+          'to-string-loader',
+          'css-loader',
+          'sass-loader']
+      }, {
+        test: /\.ts?$/,
+        use: [
+          {
+            loader: require.resolve('ts-loader'),
+          },
+        ],
+        exclude: /node_modules/
+      }
+		]
+	}
+};
